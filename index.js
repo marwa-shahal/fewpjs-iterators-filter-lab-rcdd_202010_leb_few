9a9be1d3aa0835cd611e99bc5a212024360a2c48
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-18 17:45:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+ function findMatching (driversarr, str){
+   
+   return driversarr.filter(item => {return item.toLowerCase().match(str.toLowerCase())})
+    
+ }
+
+
+function fuzzyMatch (drivers,str){
+ return drivers.filter(n => {
+  
+   return n.slice(0).match(str)
+
+   });
+   }
+   
+function matchName(drivers,str){
+  return drivers.filter(n => {
+   return n.name.match(str)});
+} 
